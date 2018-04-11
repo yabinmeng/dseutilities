9eeabe605175fe75c12aedf84c10db4d67cfca8a
yabinmeng <yabinmeng@gmail.com>
2018-04-11 11:36:19 -0500

    add "portconn.sh"
    Test whether port is open on another host

---
+++ misc/portconn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+usage () {}
+   echo "$0 <HOST_ADDRESS> <PORT_NUMBER>"
+}
+
+if [[ $# != 2 ]]; then
+   usage
+   exit
+fi
+
+SERVER=$1
+PORT=$2
+</dev/tcp/$SERVER/$PORT
+if [ "$?" -ne 0 ]; then
+   echo "Connection to $SERVER on port $PORT failed"
+   exit 1
+else
+   echo "Connection to $SERVER on port $PORT succeeded"
+   exit 0
+fi
