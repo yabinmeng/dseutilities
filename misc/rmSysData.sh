71be655089ac4ab0d6f3df3123d6447dbcb73ae4
yabinmeng <yabinmeng@gmail.com>
2019-07-17 12:55:06 -0500

    Update rmSysData.sh

--- misc/rmSysData.sh
+++ misc/rmSysData.sh
@@ -4,7 +4,13 @@ DSE_DATA_HOME=/var/lib/cassandra
 DATA_DIR=data
 SAVED_CACHES_DIR=saved_caches
 
-filelist=$(ls $DSE_DATA_HOME/$DATA_DIR/ | grep -Ev 'dse_security|system_auth|testks')
+# If multiple keyspaces are specified, use "|" as the separator with no space around.
+SYSKS_EXCLUDE_LIST="dse_security|system_auth"
+APPKS_LIST="testks|testks2"
+
+echo "$SYSKS_EXCLUDE_LIST|$APPKS_LIST"
+
+filelist=$(ls $DSE_DATA_HOME/$DATA_DIR/ | grep -Ev "$SYSKS_EXCLUDE_LIST|$APPKS_LIST")
 
 if [[ $1 == "yes" ]]; then
    echo "Deleting saved_cahces..."
